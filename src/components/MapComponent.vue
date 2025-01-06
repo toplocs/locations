59a230d4196621183ec1b03f4969b7bfdc30773f
addiinnocent <johnysmoke@web.de>
2025-01-06 16:36:37 +0100

    Get nearby location by coords and set a marker

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -6,29 +6,79 @@
 </template>
 
 <script setup lang="ts">
-	import { ref, shallowRef, onMounted } from 'vue';
+	import axios from 'axios';
+	import { ref, watch, shallowRef, onMounted } from 'vue';
 	import { GoogleMap } from '@capacitor/google-maps';
 
-	const mapRef = ref<HTMLElement>()
-	const newMap = shallowRef<GoogleMap>()
+	const mapRef = ref<HTMLElement>();
+	const map = shallowRef<GoogleMap>();
+	const location = ref(null);
 
-	async function createMap() {
+	const createMap = async () => {
 	  if (!mapRef.value) return
-	  newMap.value = await GoogleMap.create({
+	  map.value = await GoogleMap.create({
 	    id: 'locations-app',
 	    element: mapRef.value,
 	    apiKey: import.meta.env.VITE_MAPS_API_KEY,
 	    config: {
 	      center: {
-	        lat: 33.6,
-	        lng: -117.9,
+	        lat: 0,
+	        lng: 0,
 	      },
 	      zoom: 8,
 	    },
-	  })
+	  });
 	}
+
+	const getMyLocation = async () => {
+		navigator?.geolocation.getCurrentPosition((position) => {
+			const currentLocation = {
+				coordinate: {
+					lat: position.coords.latitude,
+       		lng: position.coords.longitude,
+				}
+      };
+      map.value.setCamera(currentLocation);
+      location.value = currentLocation;
+		}, (e) => {
+      console.error(e);
+    });
+	}
+
+	const fetchNearby = async ({ lat, lng }) => {
+		try {
+	    const response = await axios.get(`/api/location/byCoords?lat=${lat};&lng=${lng}`);
+
+	    return response.data;
+	  } catch (error) {
+	    console.error(error);
+	  }
+	}
+
+	watch(location, async () => {
+		const nearby = await fetchNearby(location.value.coordinate);
+		if (nearby) {
+			const markerId = await map.value.addMarker({
+			  coordinate: {
+			    lat: nearby.latitude,
+			    lng: nearby.longitude
+			  }
+			});
+			await map.value.setCamera({
+			  coordinate: {
+			    lat: nearby.latitude,
+			    lng: nearby.longitude
+			  }
+			});
+			await map.value.enableClustering();
+			await map.value.setOnMarkerClickListener((event) => {
+				console.log(event);
+			});
+		}
+	});
 
 	onMounted(async () => {
 		await createMap();
+		await getMyLocation();
 	});
 </script>
