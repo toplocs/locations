b743c0c2e5c2b1f7d897ced7775f678bd0663f5c
Felix Böhm <kontakt@felixboehm.it>
2025-01-28 13:28:13 +0100

    Update CI with env and post_clone

---
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+# Install CocoaPods
+echo "📦 Install CocoaPods"
+brew install cocoapods
+brew install node@18
+brew link node@18
+
+# Install dependencies
+# XCode Cloud is literally broken for 2 months now - https://developer.apple.com/forums/thread/738136?answerId=774510022#774510022
+npm config set maxsockets 3
+
+pnpm install --frozen-lockfile
+pnpm build
+pnpm sync:ios
