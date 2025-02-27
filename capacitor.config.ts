1be06677d95cc331e8252a0673a780779cf59071
Felix Böhm <kontakt@felixboehm.it>
2025-02-27 09:47:15 +0100

    test ci

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.toplocs.locations',
+  appId: 'com.toplocs.apps.locations',
   appName: 'locations',
   webDir: 'dist'
 };
